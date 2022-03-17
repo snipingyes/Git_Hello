4ab70fe95e81320bd7fe34fb0af00f4043bee3ce
XuJia <snipingyes@gmail.com>
2022-03-17 13:42:06 +0800

    2022.2

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,5 +3,13 @@ int main(){
 	int num1;
 	scanf ("please input a random number\n",&num1);
 	printf ("Hello Fucking World");
+	while (num1<1)
+	{
+	
+	num1=num1-1;
+	printf ("Now num1 is %d",&num1);
+
+	}
+
 	return 0;
 }
