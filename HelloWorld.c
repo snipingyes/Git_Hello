c438968182ca3c66bafeb256c7436e0aae1a151c
snipingyes <85045931+snipingyes@users.noreply.github.com>
2022-03-26 20:57:53 +0800

    Add files via upload

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
-int main(){
-	int num1;
-	scanf ("please input a random number\n",&num1);
+int main()
+{
 	printf ("Hello Fucking World");
-	while (num1<1)
-	{
-	
-	num1=num1-1;
-	printf ("Now num1 is %d",&num1);
-
-	}
-
 	return 0;
 }
